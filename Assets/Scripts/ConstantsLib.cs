c5af51fcb93dd16eb2256cede8555c05d2b76efe
ewalk002 <ewalk002@ucr.edu>
2014-05-09 14:38:54 -0700

    CC_RayTraced_Menued

--- Assets/Scripts/ConstantsLib.cs
+++ Assets/Scripts/ConstantsLib.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public static class ConstantsLib
 {
+
+// GameEngine
+	public const string GAME_ENGINE_NAME = "A - GameEngine";
 // Projectiles
 	// WBC
 	public const float WBC_PROJ_SPEED = 100f;
@@ -16,14 +19,29 @@ public static class ConstantsLib
 	public const float WBC_TURN_SPEED = 5f;
 
 // TilesMenu
-	public const int RAY_CAST_DIST = 100000;
-
 	public const float Y_OFFSET = 2.5f;
 
+	// TILE TAGS
 	public const string PLACEMENT_OPEN = "PlacementPlane_OPEN";
 	public const string PLACEMENT_CLOSED = "PlacementPlane_CLOSED";
-	public const string RAY_CAST_LAYER_NAME = "Placement";
+	public const string PLACEMENT_MENUED = "PlacementPlane_MENUED";
 
+	// MENU ICON TAGS
+	public const string WBC_ICON_TAG = "Wbc_icon";
+	public const string MUCUS_ICON_TAG = "Mucus_icon";
+	public const string MORTAR_ICON_TAG = "Mortar_icon";
+	public const string SYRINGE_ICON_TAG = "Syringe_icon";
+	public const string TESLAS_ICON_TAG = "Tesla_icon";
+
+	// LAYER NAMES
+	public const string RAY_CAST_TILES = "Placement";
+	public const string RAY_CAST_WBC = "wbc_icon";
+	public const string RAY_CAST_MUCUS = "mucus_icon";
+	public const string RAY_CAST_MORTAR = "mortar_icon";
+	public const string RAY_CAST_SYRINGE = "syringe_icon";
+	public const string RAY_CAST_TESLA = "tesla_icon";
+
+	// PREFAB PATHS
 	public const string TOWER_MENU_PATH = "Prefabs/Menus/towerMenu";
 	public const string WBC_PATH = "Prefabs/TowerPrefabs/wbc";
 	public const string MUCUS_PATH = "Prefabs/TowerPrefabs/mucus";
--- Assets/Scripts/Tiles/ClickPlaneMenu.cs
+++ Assets/Scripts/Tiles/ClickPlaneMenu.cs
@@ -3,37 +3,47 @@ using System.Collections;
 
 public class ClickPlaneMenu : MonoBehaviour {
 
-	public GUIStyle PriceStyle;
-	public Texture wbcTexture;
-	public Texture mucusTexture;
-	public Texture syringeTexture;
-	public Texture mortarTexture;
-	public Texture teslaTexture;
-	public bool valid;
-	public bool CreateTurretMenu = false;
-	public bool rayCast = false;
-	
 	private GameObject wbcTurret;
 	private GameObject mucusTurret;
 	private GameObject syringeTurret;
 	private GameObject mortarTurret;
 	private GameObject teslaTurret;
 	private GameObject towerMenu;
-	
-	public bool AlreadyCreated = false;
-	public bool gui = true;
 
+	private GameObject MenuManager;
 
-
-
-	private GameObject lastHitObj = null;
+	private GameObject lastHoverObj = null;
+	private GameObject lastMenuedObj = null;
 	private GameObject menu = null;
+	private GameObject menuTowerIcon = null;
+
 	private Material originalMat;
 	public Material hoverMat;
+	public Material menuedMat;
 
+	private int placementLayerMask;
+	private int wbcLayerMask;
+	private int mucusLayerMask;
+	private int mortarLayerMask;
+	private int syringLayerMask;
+	private int teslaLayerMask;
+	private int towerMenuLayerMask;
+
+	GameObject wbc;
 
 	// Use this for initialization
 	void Start () {
+		wbcLayerMask = 1 << (LayerMask.NameToLayer (ConstantsLib.RAY_CAST_WBC));
+		mucusLayerMask = 1 << (LayerMask.NameToLayer (ConstantsLib.RAY_CAST_MUCUS));
+		mortarLayerMask = 1 << (LayerMask.NameToLayer (ConstantsLib.RAY_CAST_MORTAR));
+		syringLayerMask = 1 << (LayerMask.NameToLayer (ConstantsLib.RAY_CAST_SYRINGE));
+		teslaLayerMask = 1 << (LayerMask.NameToLayer (ConstantsLib.RAY_CAST_TESLA));
+		placementLayerMask = 1 << (LayerMask.NameToLayer (ConstantsLib.RAY_CAST_TILES));
+
+
+		towerMenuLayerMask = wbcLayerMask | mucusLayerMask | mortarLayerMask | 
+			syringLayerMask | teslaLayerMask;
+
 		originalMat = gameObject.renderer.material;
 		towerMenu = (GameObject)Resources.Load ( ConstantsLib.TOWER_MENU_PATH, typeof(GameObject));
 		wbcTurret = (GameObject)Resources.Load ( ConstantsLib.WBC_PATH, typeof(GameObject));
@@ -41,6 +51,8 @@ public class ClickPlaneMenu : MonoBehaviour {
 		syringeTurret = (GameObject)Resources.Load ( ConstantsLib.SYRINGE_PATH, typeof(GameObject));
 		mortarTurret = (GameObject)Resources.Load ( ConstantsLib.MORTAR_PATH, typeof(GameObject));
 		teslaTurret = (GameObject)Resources.Load ( ConstantsLib.TESLA_PATH, typeof(GameObject));
+
+		MenuManager = GameObject.Find (ConstantsLib.GAME_ENGINE_NAME);
 	}
 	  
 	// Update is called once per frame
@@ -48,61 +60,141 @@ public class ClickPlaneMenu : MonoBehaviour {
 		var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 		var hit = new RaycastHit ();
 
-		/*
-		if( Physics.Raycast( ray, out hit ) )
+		// tower placement
+		if( menu != null )
 		{
-			print ( "true " + hit.point );
+			// cast to the tiles ( no click detect )
+			if( Physics.Raycast( ray, out hit, Mathf.Infinity, towerMenuLayerMask ) )
+			{				
+				if( Input.GetMouseButtonDown( 0 ) )
+				{
+					//Debug.Log ( "Hit the menu" );
+					
+					menuTowerIcon = hit.collider.gameObject; // replace last objecct with what we just hit
+					Vector3 hitPoint = lastMenuedObj.transform.position;
+					//hitPoint.y += ConstantsLib.Y_OFFSET;
+					
+					Destroy( menu );
+					menu = null;
+					
+					if( menuTowerIcon.tag == ConstantsLib.WBC_ICON_TAG )
+					{
+						wbc = (GameObject)Instantiate( wbcTurret, hitPoint, 
+						                              Quaternion.identity );
+					}
+					else if( menuTowerIcon.tag == ConstantsLib.MUCUS_ICON_TAG )
+					{
+						wbc = (GameObject)Instantiate( mucusTurret, hitPoint, 
+						                              Quaternion.identity );
+					}
+					else if( menuTowerIcon.tag == ConstantsLib.MORTAR_ICON_TAG )
+					{
+						wbc = (GameObject)Instantiate( mortarTurret, hitPoint, 
+						                              Quaternion.identity );
+					}
+					else if( menuTowerIcon.tag == ConstantsLib.SYRINGE_ICON_TAG )
+					{
+						wbc = (GameObject)Instantiate( syringeTurret, hitPoint, 
+						                              Quaternion.identity );
+					}
+					else if( menuTowerIcon.tag == ConstantsLib.TESLAS_ICON_TAG )
+					{
+						wbc = (GameObject)Instantiate( teslaTurret, hitPoint, 
+						                              Quaternion.identity );
+					}
+				}
+			}
 		}
-		*/
 
-		//print ( "Layer" + LayerMask.NameToLayer (ConstantsLib.RAY_CAST_LAYER_NAME));
-
-		var rayCast = Physics.Raycast (ray, out hit, ConstantsLib.RAY_CAST_DIST, 
-		                              LayerMask.NameToLayer (ConstantsLib.RAY_CAST_LAYER_NAME));
-
-		print (rayCast.ToString());
-
-		// cast to the tiles
-		if( Physics.Raycast( ray, out hit, ConstantsLib.RAY_CAST_DIST, 
-		                    LayerMask.NameToLayer( ConstantsLib.RAY_CAST_LAYER_NAME ) ) )
+		// cast to the tiles ( no click detect )
+		if( Physics.Raycast( ray, out hit, Mathf.Infinity, placementLayerMask ) )
 		{
-
-			if( lastHitObj != null )
+			// revert tile to orig material if not highlighted as menued
+			if( lastHoverObj != null && lastHoverObj != lastMenuedObj )
 			{
-				lastHitObj.renderer.material = originalMat;
+				lastHoverObj.renderer.material = originalMat;
 			}
 
-			lastHitObj = hit.collider.gameObject; // replace last objecct with what we just hit
-			originalMat = lastHitObj.renderer.material; // store new planes atarting material
-			lastHitObj.renderer.material = hoverMat; // set to hover material
+			lastHoverObj = hit.collider.gameObject; // replace last objecct with what we just hit
+
+			//Debug.Log ( lastHoverObj == lastMenuedObj );
+
+			if( lastHoverObj != lastMenuedObj )
+			{
+
+				lastHoverObj.renderer.material = hoverMat; // set to hover material
+			//	MenuManager.GetComponent<MenuManager>().menuMade = false;
+			}
 		}
 		else // did not hit anything. Set tiles to original material, and destroy menu
 		{
-			if( lastHitObj != null )
+			if( lastHoverObj != null )
 			{
-				lastHitObj.renderer.material = originalMat;
-				lastHitObj = null;
+				// if we arent hovering over the menued object
+				if( lastMenuedObj != lastHoverObj )
+				{
+					lastHoverObj.renderer.material = originalMat;
+					lastHoverObj = null;
+				}
+			}
+		}
+
+		//Debug.Log (menuMade);
+
+		// drop turrets on click
+		if( Input.GetMouseButtonDown( 0 ) && lastHoverObj != null  ) // if LEFT mouse down, and object is selected
+		{
+			if( lastMenuedObj != null )
+			{
+				lastMenuedObj.renderer.material = originalMat;
 			}
 
+			lastMenuedObj = lastHoverObj.gameObject;
+
+			if( lastMenuedObj.tag == ConstantsLib.PLACEMENT_OPEN && 
+			   				!MenuManager.GetComponent<MenuManager>().menuMade ) //&& menu != null )
+			{
+				lastMenuedObj.renderer.material = menuedMat; // set clicked tile to green color
+
+				Vector3 hitPoint = lastMenuedObj.transform.position; // position of object we hit
+				hitPoint.y += ConstantsLib.Y_OFFSET; // raise it off the ground a bit
+
+				if( menu != null )
+				{
+					Destroy( menu );
+					menu = null;
+				}
+
+				Vector3 atCamera = Camera.main.transform.position - lastMenuedObj.transform.position;
+				//atCamera.y += ConstantsLib.Y_OFFSET;
+
+				menu = ( GameObject )Instantiate( towerMenu, hitPoint, 
+				                                 Quaternion.LookRotation( atCamera ) );
+				//Debug.Log ( "MENU MADE " + menuMade );
+
+				Debug.Log ( transform.position );
+
+				MenuManager.GetComponent<MenuManager>().menuMade = true;
+			}
+			//else if( lastHoverObj.tag == ConstantsLib.
+		}
+		else if ( Input.GetMouseButtonDown( 0 ) && lastHoverObj == null ) 
+		{
 			if( menu != null )
 			{
 				Destroy( menu );
 				menu = null;
 			}
-		}
 
-		// drop turrets on click
-		if( Input.GetMouseButtonDown( 0 ) && lastHitObj != null ) // if LEFT mouse down, and object is selected
-		{
-			if( lastHitObj.tag == ConstantsLib.PLACEMENT_OPEN && menu != null )
+			if( lastMenuedObj != null )
 			{
-				Vector3 hitPoint = lastHitObj.transform.position; // position of object we hit
-				hitPoint.y += ConstantsLib.Y_OFFSET; // raise it off the ground a bit
-
-				menu = ( GameObject )Instantiate( towerMenu, hitPoint, Quaternion.identity );
+				lastMenuedObj.renderer.material = originalMat;
+				lastMenuedObj = null;
 			}
-			//else if( lastHitObj.tag == ConstantsLib.
 		}
+
+
+
 
 	}
 }
